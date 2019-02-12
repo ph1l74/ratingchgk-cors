4ebd9ff6bbdc3f267d9abf869d567fcb052196ef
Filat Astakhov <astakhovfilat@gmail.com>
2019-02-12 11:33:07 +0500

    необходимые файлы для деплоя на хероку

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return '<html><head><meta charset=utf-8" /></head><body><ul><li><a href="/rating">rating</a></li><li><a href="/tournaments">tournaments</a></li></ul></body></html>'
+    return '<html><head><meta charset=utf-8" /></head>' \
+           '<body><ul>' \
+           '<li><a href="/rating">rating</a></li>' \
+           '<li><a href="/tournaments">tournaments</a></li>' \
+           '<li><a href="/teamrating">teamrating</a></li>' \
+           '</ul>' \
+           '</body></html>'
 
 
 class Rating(Resource):
@@ -71,8 +77,48 @@ class Tournaments(Resource):
         return self.tournaments
 
 
+class TeamRating(Resource):
+    def __init__(self):
+        self.team_rating = {
+            'current_rating': '',
+            'current_rating_position' : '',
+            'last_revision': '',
+            'prev_rating': '',
+            'prev_rating_position': ''
+        }
+
+    @staticmethod
+    def convert_request(request_string):
+        req = requests.get(request_string)
+        result = req.json()
+        return result
+
+    def get_current_rating(self):
+        last_b = self.convert_request('http://rating.chgk.info/api/teams/5723/rating/b.json')
+        self.team_rating['current_rating'] = last_b['rating']
+        self.team_rating['current_rating_position'] = last_b['rating_position']
+        self.team_rating['last_revision'] = last_b['idrelease']
+        return print('current rating got {0}, {1}, {2}'.format(self.team_rating['current_rating'],
+                                                               self.team_rating['current_rating_position'],
+                                                               self.team_rating['last_revision']))
+
+    def get_prev_rating(self):
+        prev_revision = int(self.team_rating['last_revision']) - 1
+        prev_b = self.convert_request('http://rating.chgk.info/api/teams/5723/rating/{}.json'.format(prev_revision))
+        self.team_rating['prev_rating'] = prev_b['rating']
+        self.team_rating['prev_rating_position'] = prev_b['rating_position']
+        return print('prev rating got: {0}, {1}'.format(self.team_rating['prev_rating'],
+                                                        self.team_rating['prev_rating_position']))
+
+    def get(self):
+        self.get_current_rating()
+        self.get_prev_rating()
+        return self.team_rating
+
+
 api.add_resource(Rating, '/rating')
 api.add_resource(Tournaments, '/tournaments')
+api.add_resource(TeamRating, '/teamrating')
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port='5000')
